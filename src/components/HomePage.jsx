1c32e41cb8012197834b1699fe2c4e6c83dd6a53
starydv7 <pawanstar72926@gmail.com>
2022-08-03 01:15:50 +0530

    align

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -32,39 +32,33 @@ const HomePage = () => {
           <div className={styles.follow}>
               <p>Follow Us on</p>
               <div className={styles.social}>
-              <div className={styles.youtube}>
-               <a
+         <div style={{marginTop:"10px",marginLeft:"10px"}}>
+             <a
           href="https://www.youtube.com/c/jamesqquick"
           className="youtube social"
                   >
                       
           <FontAwesomeIcon icon={faYoutube} size="1x" />
-                  </a>
-              </div>
-              <div className={styles.facebook}>
-        <a
+            </a>
+            <a
           href="https://www.facebook.com/learnbuildteach/"
           className="facebook social"
         >
           <FontAwesomeIcon icon={faFacebook} size="1x" />
-                  </a>
-              </div>
-              <div className='insta'>
-        <a
+            </a>
+              <a
           href="http://www.instagram.com/larnbuildteach"
           className="instagram social"
         >
           <FontAwesomeIcon icon={faInstagram} size="1x"color='rgb(51, 197, 25,0.7)' />
-                  </a>
-              </div>
-              <div className={styles.twitter}>
-        <a href="https://wwww.twitter.com" className="twitter social">
+            </a>
+             <a href="https://wwww.twitter.com" className="twitter social">
           <FontAwesomeIcon icon={faTwitter} size="1x"color='rgb(51, 197, 25,0.7)' />
                   </a>
-                  </div>
+          </div>
                   </div>
       </div>
-       
+       <br></br>
           <About />
           <Talent />
           <Digital />
